6a2d7bbc9b9fcbcb3dc23c92b866036babdc86a1
nizrum <nizrum215@yandex.ru>
2024-10-24 13:32:44 +0300

    feat: add alt text to images

--- scripts/index.js
+++ scripts/index.js
@@ -23,21 +23,23 @@ const popupCaption = imagePopup.querySelector(".popup__caption");
 
 function createCard(name, link) {
 	const card = cardTemplate.cloneNode(true);
-    const cardImage = card.querySelector(".card__image");
-    const cardTitle = card.querySelector(".card__title");
+	const cardImage = card.querySelector(".card__image");
+	const cardTitle = card.querySelector(".card__title");
 	cardTitle.textContent = name;
 	cardImage.src = link;
+	cardImage.alt = name;
 	card.querySelector(".card__like-button").addEventListener("click", (evt) => {
-        evt.target.classList.toggle('card__like-button_is-active');
-    });
-    card.querySelector('.card__delete-button').addEventListener('click', (evt) => {
-        evt.target.closest('.card').remove();
-    });
-    cardImage.addEventListener('click', () => {
-        popupImage.src = cardImage.src;
-        popupCaption.textContent = cardTitle.textContent;
-        openModal(imagePopup);
-    });
+		evt.target.classList.toggle("card__like-button_is-active");
+	});
+	card.querySelector(".card__delete-button").addEventListener("click", (evt) => {
+		evt.target.closest(".card").remove();
+	});
+	cardImage.addEventListener("click", () => {
+		popupImage.src = cardImage.src;
+		popupImage.alt = cardTitle.textContent;
+		popupCaption.textContent = cardTitle.textContent;
+		openModal(imagePopup);
+	});
 	return card;
 }
 
@@ -89,7 +91,7 @@ popupCloseButtons.forEach((button) => {
 });
 
 popups.forEach((popup) => {
-    popup.classList.add('popup_is-animated');
+	popup.classList.add("popup_is-animated");
 });
 
 profileEditButton.addEventListener("click", openProfilePopup);
