eb6d485c1a665626de656f990e0c28cab0575c45
nizrum <nizrum215@yandex.ru>
2024-10-27 19:50:40 +0300

    feat: add validation to popup forms

--- scripts/index.js
+++ scripts/index.js
@@ -21,6 +21,15 @@ const imagePopup = document.querySelector(".popup_type_image");
 const popupImage = imagePopup.querySelector(".popup__image");
 const popupCaption = imagePopup.querySelector(".popup__caption");
 
+const validationSettings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}
+
 function createCard(name, link) {
 	const card = cardTemplate.cloneNode(true);
 	const cardImage = card.querySelector(".card__image");
@@ -80,6 +89,65 @@ function handleCardFormSubmit(evt) {
 	closeModal(cardPopup);
 }
 
+function showInputError(formElement, inputElement, errorMessage, settings) {
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+	inputElement.classList.add(settings.inputErrorClass);
+	errorElement.textContent = errorMessage;
+	errorElement.classList.add(settings.errorClass);
+}
+
+function hideInputError(formElement, inputElement, settings) {
+	const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+	inputElement.classList.remove(settings.inputErrorClass);
+	errorElement.classList.remove(settings.errorClass);
+	errorElement.textContent = "";
+}
+
+function checkInputValidity(formElement, inputElement, settings) {
+	if (!inputElement.validity.valid) {
+		showInputError(formElement, inputElement, inputElement.validationMessage, settings);
+	} else {
+		hideInputError(formElement, inputElement, settings);
+	}
+}
+
+function hasInvalidInput(inputList) {
+	return inputList.some((input) => input.validity.valid == false);
+}
+
+function toggleButtonState(inputList, buttonElement, settings) {
+	if (hasInvalidInput(inputList)) {
+    buttonElement.setAttribute("disabled", "true");
+		buttonElement.classList.add(settings.inactiveButtonClass);
+	} else {
+    buttonElement.removeAttribute("disabled");
+		buttonElement.classList.remove(settings.inactiveButtonClass);
+	}
+}
+
+function setEventListeners(formElement, settings) {
+	const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+	const buttonElement = formElement.querySelector(settings.submitButtonSelector);
+	toggleButtonState(inputList, buttonElement, settings);
+	inputList.forEach((inputElement) => {
+		inputElement.addEventListener("input", function () {
+			checkInputValidity(formElement, inputElement, settings);
+			toggleButtonState(inputList, buttonElement, settings);
+		});
+	});
+}
+
+function enableValidation(settings) {
+	const formList = Array.from(document.querySelectorAll(settings.formSelector));
+	formList.forEach((formElement) => {
+		formElement.addEventListener("submit", function (evt) {
+			evt.preventDefault();
+		});
+
+		setEventListeners(formElement, settings);
+	});
+}
+
 initialCards.forEach((card) => {
 	placesList.append(createCard(card.name, card.link));
 });
@@ -98,3 +166,5 @@ profileEditButton.addEventListener("click", openProfilePopup);
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
 cardAddButton.addEventListener("click", openCardAddPopup);
 cardFormElement.addEventListener("submit", handleCardFormSubmit);
+
+enableValidation(validationSettings);
